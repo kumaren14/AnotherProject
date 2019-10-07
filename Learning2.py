cff25e14e0117fe761cea0f9c78476a69f35ea82
kumaren14 <kumaren.a@gmail.com>
2019-10-07 15:19:03 -0500

    more strings

--- Learning2.py
+++ Learning2.py
@@ -1,1 +1,9 @@
-# hello
+# more strings and tests
+
+x = "There are %d types of people" % 10
+binary = "binary"
+doNot = "don't"
+y = "Those who know %s and those who %s" % (binary, doNot)
+
+print(x)
+print(y)
